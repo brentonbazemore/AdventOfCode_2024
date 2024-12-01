86829bb15b67566eb2772623a26223307cce81c5
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-01 00:18:00 -0500

    Finish Day 1 - Part 2

--- days/1/2.ts
+++ days/1/2.ts
@@ -13,31 +13,11 @@ data.forEach(row => {
     right.push({ value: +r, seen: false });
 });
 
-const min = (ar: { value: number, seen: boolean}[]) => {
-    let localMin = Infinity;
-    let pos = -1;
-    for (let i = 0; i < ar.length; i++) {
-        if (ar[i].value < localMin && !ar[i].seen) {
-            localMin = ar[i].value;
-            pos = i;
-        }
-    }
-    ar[pos].seen = true;
-    return { ...ar[pos], pos };
-}
-let leftMin;
-let rightMin;
 let sum = 0;
-for (let i = 0; i < data.length; i++) {
-    leftMin = min(left);
-    rightMin = min(right);
-    // console.log(leftMin, rightMin)
-
-    const dist = Math.abs(leftMin.value - rightMin.value);
-    console.log(dist);
-    sum += dist;
-}
-
+left.forEach(l => {
+    const count = right.filter(r => r.value === l.value).length;
+    sum += l.value * count;
+});
 
 console.log(sum);
 
