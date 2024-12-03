4bd28d3931474bd4ef3f16a2b92fe5058cf28e19
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-03 06:43:10 -0500

    Finish Day 3 - Part 2

--- days/3/2.ts
+++ days/3/2.ts
@@ -2,15 +2,20 @@ import '../../types/helper.d.ts';
 
 const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
-const data = rawData.split('\n');
+const data = rawData.replaceAll('\n', '');
 
-const lines = data.map(line => line.matchAll(/mul\([\d]{1,3},[\d]{1,3}\)/g).toArray());
+const enables = data.matchAll(/do\(\)/g).toArray();
+const disables = data.matchAll(/don't\(\)/g).toArray();
+const muls = data.matchAll(/mul\([\d]{1,3},[\d]{1,3}\)/g).toArray();
+
+const toggles = [{[0]: 'do()', index: 0}, ...enables, ...disables].sort((a, b) => a.index - b.index);
 
 let sum = 0;
-lines.forEach(line => {
-    line.forEach(inst => {
-        const [a, b] = inst[0].replace('mul(', '').replace(')', '').split(',').map(Number);
+muls.forEach(inst => {
+    const isEnabled = toggles.findLast((toggle) => toggle.index < inst.index)?.[0] === 'do()';
+    const [a, b] = inst[0].replace('mul(', '').replace(')', '').split(',').map(Number);
+    if (isEnabled) {
         sum += a * b;
-    })
+    }
 });
 console.log(sum);
