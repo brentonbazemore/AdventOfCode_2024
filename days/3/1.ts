97e0950ad6651e8d967c0d2a106b0da0c6020c1d
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-03 06:29:24 -0500

    Finish Day 3 - Part 1

--- days/3/1.ts
+++ days/3/1.ts
@@ -4,3 +4,13 @@ const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
 const data = rawData.split('\n');
 
+const lines = data.map(line => line.matchAll(/mul\([\d]{1,3},[\d]{1,3}\)/g).toArray());
+
+let sum = 0;
+lines.forEach(line => {
+    line.forEach(inst => {
+        const [a, b] = inst[0].replace('mul(', '').replace(')', '').split(',').map(Number);
+        sum += a * b;
+    })
+});
+console.log(sum);
