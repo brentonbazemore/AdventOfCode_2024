b47fece6add5279b06cfc81896f6d7a9930a0b35
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-04 08:20:01 -0500

    Finish Day 4 - Part 2

--- days/4/2.ts
+++ days/4/2.ts
@@ -4,7 +4,7 @@ const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
 const data = rawData.split('\n');
 
-const PATTERN = 'XMAS'.split('');
+const PATTERN = 'MAS'.split('');
 const up = (x: number, y: number) => PATTERN.every((char, i) => data[y - i]?.[x] === char);
 const down = (x: number, y: number) => PATTERN.every((char, i) => data[y + i]?.[x] === char);
 const left = (x: number, y: number) => PATTERN.every((char, i) => data[y]?.[x - i] === char);
@@ -17,10 +17,22 @@ const upLeft = (x: number, y: number) => PATTERN.every((char, i) => data[y - i]?
 let sum = 0;
 for (let y = 0; y < data.length; y++) {
   for (let x = 0; x < data[0].length; x++) {
-    const root = data[y][x];
+    const bottomLeft = [x - 1, y + 1];
+    const bottomRight = [x + 1, y + 1];
+    const topRight = [x + 1, y - 1];
+    const topLeft = [x - 1, y - 1];
+    const crossingPairs: [Function, Function, any, any][] = [
+      [upRight, downRight, bottomLeft, topLeft],
+      [upLeft, upRight, bottomRight, bottomLeft],
+      [upLeft, downLeft, bottomRight, topRight],
+      [downLeft, downRight, topRight, topLeft],
+    ];
 
-    const matches = [up, down, left, right, upRight, downRight, downLeft, upLeft].filter(func => func(x, y)).length;
-    sum += matches;
+    const xShape = crossingPairs.some(([dir1, dir2, root1, root2]) => dir1(root1[0], root1[1]) && dir2(root2[0], root2[1]));
+
+    if (xShape) {
+      sum += 1;
+    }
   }
 }
 console.log(sum);
