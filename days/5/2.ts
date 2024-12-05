dc2c9451d6f5b2ce5dc65f78d36f42044e140c27
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-05 08:23:15 -0500

    Finish Day 5 - Part 2

--- days/5/2.ts
+++ days/5/2.ts
@@ -7,7 +7,26 @@ const [rawRules, rawPages] = rawData.split('\n\n');
 const pages = rawPages.split('\n').map(rawPage => rawPage.split(',').map(Number));
 const rules = rawRules.split('\n').map(rawRule => rawRule.split('|').map(Number));
 
-let sum = 0;
+const incorrectOrders: number[][] = [];
+const doSort = (pageList: number[], validRules: [number, number][]) => {
+    const sorted = pageList.toSorted((a, b) => {
+        for (let k = 0; k < validRules.length; k++) {
+            const [pre, post] = validRules[k];
+            if (a === pre && b === post) {
+                return -1;
+            }
+
+            if (a === post && b === pre) {
+                return 1
+            }
+        }
+
+        return 0;
+    });
+    incorrectOrders.push(sorted);
+}
+
+
 pageLoop:
 for (let i = 0; i < pages.length; i++) {
     const pageList = pages[i];
@@ -27,10 +46,13 @@ for (let i = 0; i < pages.length; i++) {
     for (let j = 0; j < validRules.length; j++) {
         const [pre, post] = validRules[j];
         if (pageLookup.get(pre)! >= pageLookup.get(post)!) {
+            doSort(pageList, validRules);
             continue pageLoop;
         }
     }
-
-    sum += pageList[Math.floor(pageList.length / 2)]
 }
+let sum = 0;
+incorrectOrders.forEach(list => {
+    sum += list[Math.floor(list.length / 2)]
+})
 console.log(sum);
