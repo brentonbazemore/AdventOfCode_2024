347dfddc0e9748307d17c9df3e699410eb2e96a9
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-12 00:24:07 -0500

    Init Day 12

---
+++ days/12/1.ts
@@ -0,0 +1,6 @@
+import '../../types/helper.d.ts';
+
+const inputFile = process.argv[2];
+const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
+const data = rawData.split('\n');
+
