fa9db1350e5ea7750d6b077a08535654b551961b
brentonbazemore <brenton.bazemore@gmail.com>
2024-12-19 08:46:28 -0500

    Finish Day 19 - Part 1

--- days/19/1.ts
+++ days/19/1.ts
@@ -2,5 +2,40 @@ import '../../types/helper.d.ts';
 
 const inputFile = process.argv[2];
 const rawData = await Bun.file(`${import.meta.dir}/${inputFile || 'input.txt'}`).text();
-const data = rawData.split('\n');
+const [rawOptions, rawPatterns] = rawData.split('\n\n');
 
+const options = rawOptions.split(', ');
+const patterns = rawPatterns.split('\n');
+
+// dfs
+const check = (pattern: string, cache: Map<string, number>) => {
+  if (cache.has(pattern)) {
+    return cache.get(pattern)!;
+  }
+
+  if (pattern.length === 0) {
+    return 1;
+  }
+
+  let count = 0;
+  options.forEach((option) => {
+    const piece = pattern.substring(0, option.length);
+    if (piece === option) {
+      count += check(pattern.substring(option.length), cache);
+    }
+  });
+
+  cache.set(pattern, count);
+
+  return count;
+};
+
+let sum = 0;
+patterns.forEach((pattern, i) => {
+  // console.log('Pattern', i);
+  if (check(pattern, new Map<string, number>())) {
+    sum += 1;
+  }
+});
+
+console.log(sum);
